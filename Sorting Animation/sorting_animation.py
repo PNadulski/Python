3ab063266bfcd02afc7119afbb37d0c7726ebfc3
PNadulski <38704326+PNadulski@users.noreply.github.com>
2021-10-21 14:28:03 +0200

    tracking actual scope of algoritm

--- Sorting Animation/sorting_animation.py
+++ Sorting Animation/sorting_animation.py
@@ -4,29 +4,30 @@ import scipy as sp
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-class TrackedArray:
-    def __init__(self, arr):
+class TrackedArray: # 
+    def __init__(self, arr, kind="not full"):
         self.arr = np.copy(arr)
+        self.kind = kind
         self.reset()
 
     def reset(self):
         self.indices = []
         self.values = []
-        self.acces_type = []
+        self.access_type = []
         self.full_copies = []
-        self.full_copies.append(np.copy(self.arr))
-    
+
     def track(self, key, access_type):
         self.indices.append(key)
         self.values.append(self.arr[key])
-        self.acces_type.append(access_type)
-        self.full_copies.append(np.copy(self.arr))
+        self.access_type.append(access_type)
+        if self.kind == "full":
+            self.full_copies.append(np.copy(self.arr))
 
     def GetActivity(self, idx=None):
         if isinstance(idx, type(None)):
-            return [(i, op) for (i, op) in zip(self.indices, self.acces_type)]
+            return [(i, op) for (i, op) in zip(self.indices, self.access_type)]
         else:
-            return (self.indices[idx], self.acces_type[idx])
+            return (self.indices[idx], self.access_type[idx])
 
     def __getitem__(self, key):
         self.track(key, 'get')
@@ -43,7 +44,13 @@ def update(frame):
     steps_counter.set_text(f'Steps = {frame}')
     for (rectangle, height) in zip(container.patches, arr.full_copies[frame]):
         rectangle.set_height(height)
-        rectangle.set_color('#1f77b4')
+        rectangle.set_color('#9400D3')
+    
+    idx, op = arr.GetActivity(frame)
+    if op == "get":
+        container.patches[idx].set_color("#FFA500")
+    elif op == "set":
+        container.patches[idx].set_color("#FFD700")
     return (*container, steps_counter)
 
 plt.rcParams['figure.figsize'] = (12,8)
@@ -53,7 +60,7 @@ N = 50
 arr = np.round(np.linspace(0,1000,N),0)
 np.random.seed(0)
 np.random.shuffle(arr)
-arr = TrackedArray(arr)
+arr = TrackedArray(arr, 'full')
 
 ################################################
 ################# Bubble Sort ##################
@@ -62,9 +69,7 @@ def bubble_sort(arr):
    for iter_num in range(len(arr)-1,0,-1):
       for idx in range(iter_num):
          if arr[idx]>arr[idx+1]:
-            temp = arr[idx]
-            arr[idx] = arr[idx+1]
-            arr[idx+1] = temp
+            arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
 
 ################################################
 ################# Quick Sort ###################
@@ -79,28 +84,24 @@ def partition(arr, lo, hi):
     pivot, i = arr[hi], lo
     for j in range(lo, hi):
         if arr[j] < pivot:
-            temp = arr[i]
-            arr[i], arr[j] = arr[j], temp
+            arr[i], arr[j] = arr[j], arr[i]
             i+=1
-    temp = arr[i]
-    arr[i], arr[hi] = arr[hi], temp
+    arr[i], arr[hi] = arr[hi], arr[i]
     return i
 
 ################################################
 ################# Merge Sort ###################
 ################################################
 def merge(arr, l, m, r):
-	n1 = m - l + 1
-	n2 = r - m
-	L = [0] * (n1)
-	R = [0] * (n2)
+	n1 = m-l+1
+	n2 = r-m
+	L, R = [0]*(n1), [0]*(n2) 
 	for i in range(0, n1):
-		L[i] = arr[l + i]
+		L[i] = arr[l+i]
 	for j in range(0, n2):
-		R[j] = arr[m + 1 + j]
-	i = 0	 
-	j = 0	 
-	k = l
+		R[j] = arr[m+1+j]
+	i, j, k = 0, 0, l	 
+
 	while i < n1 and j < n2:
 		if L[i] <= R[j]:
 			arr[k] = L[i]
@@ -134,9 +135,7 @@ def instertion_sort(arr):
     while i<len(arr):
         j=i
         while (j>0 and arr[j-1] > arr[j]):
-            temp=arr[j-1]
-            arr[j-1] = arr[j]
-            arr[j] = temp
+            arr[j-1], arr[j] = arr[j], arr[j-1]
             j-=1
         i+=1
     return arr
@@ -206,3 +205,4 @@ ax.set(xlabel='Index', ylabel='Value', title=f'{sorter}')
 steps_counter = ax.text(0,1000, "")
 ani = FuncAnimation(fig, update, frames=range(len(arr.full_copies)), blit=True, interval=1000./FPS, repeat=False)
 plt.show()
+
